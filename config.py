8768ac71497d91e1078b2e454696425bfbc13ce9
刘张培(10027543) <liuzp-a@glodon.com>
2020-08-19 15:21:22 +0800

    ...

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ DIR = os.getcwd()
 DEFAULT_QR = 'QR.png'
 TIMEOUT = (10, 60)
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
